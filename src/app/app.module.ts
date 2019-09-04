2b520cd6afd350a2509b977e23c349fa3208478c
Maksim Paulouski <AksimTech@gmail.com>
2019-09-04 16:55:32 +0300

    creating service error-handler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
+import { InternalServerComponent } from './error-pages/internal-server/internal-server.component';
 
 import { EnvironmentUrlService } from './shared/services/environment-url.service';
 import { RepositoryService } from './shared/services/repository.service';
-import { InternalServerComponent } from './error-pages/internal-server/internal-server.component';
+import { ErrorHandlerService } from './shared/services/error-handler.service';
+
 
 @NgModule({
   declarations: [
@@ -34,7 +36,8 @@ import { InternalServerComponent } from './error-pages/internal-server/internal-
   ],
   providers: [
     EnvironmentUrlService,
-    RepositoryService
+    RepositoryService,
+    ErrorHandlerService
   ],
   bootstrap: [AppComponent]
 })
