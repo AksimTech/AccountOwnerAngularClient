071bedc0abe726b9459c482b4b7a4b0f445f56a6
Maksim Paulouski <AksimTech@gmail.com>
2019-09-09 17:09:52 +0300

    created owner-create.component

--- src/app/owner/owner.module.ts
+++ src/app/owner/owner.module.ts
@@ -2,25 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { OwnerListComponent } from './owner-list/owner-list.component';
 import { OwnerDetailsComponent } from './owner-details/owner-details.component';
 import { AccountListComponent } from './owner-details/account-list/account-list.component';
+import { OwnerCreateComponent } from './owner-create/owner-create.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       { path: 'list', component: OwnerListComponent },
-      { path: 'details/:id', component: OwnerDetailsComponent }
+      { path: 'details/:id', component: OwnerDetailsComponent },
+      { path: 'create', component: OwnerCreateComponent}
     ])
   ],
   declarations: [
     OwnerListComponent,
     OwnerDetailsComponent,
-    AccountListComponent
+    AccountListComponent,
+    OwnerCreateComponent
   ]
 })
 export class OwnerModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
 import { SuccessModalComponent } from './modals/success-modal/success-modal.component';
+import { DatepickerDirective } from './directives/datepicker.directive';
 
 @NgModule({
   declarations: [
   ErrorModalComponent,
-  SuccessModalComponent
+  SuccessModalComponent,
+  DatepickerDirective
 ],
   imports: [
     CommonModule
   ],
   exports: [
     ErrorModalComponent,
-    SuccessModalComponent
+    SuccessModalComponent,
+    DatepickerDirective
   ]
 })
 export class SharedModule { }
